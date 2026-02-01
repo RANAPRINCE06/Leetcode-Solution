206aed9d5d1663dbeeaaf6ea87efc5fba332d1bf
Prince <rana.prince1806@gmail.com>
2026-02-01 20:42:45 +0530

    Add files via upload

---
+++ leetcode3010.java
@@ -0,0 +1,12 @@
+// public class leetcode3010 {
+    
+// }
+class Solution {
+    public int minimumCost(int[] nums) {
+       int ans=nums[0];
+       Arrays.sort(nums,1,nums.length);
+       ans+=nums[1];
+       ans+=nums[2];
+       return ans;
+    }
+}
