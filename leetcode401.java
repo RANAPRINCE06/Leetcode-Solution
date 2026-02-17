bcb4706fda1e302eb98a82bef9624f21c0da64ca
Prince <rana.prince1806@gmail.com>
2026-02-17 22:11:16 +0530

    Add files via upload

---
+++ leetcode401.java
@@ -0,0 +1,19 @@
+// public class leetcode401 {
+    
+// }
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+       List<String> ans = new ArrayList<>();
+        for (int i = 0; i < 12; i++)
+            for (int j = 0; j < 60; j++) {
+                if (Integer.bitCount(i) + Integer.bitCount(j) == turnedOn) {
+                    if (j < 10) {
+                        ans.add(i + ":" + "0" + j);
+                    }
+                    else
+                        ans.add(i + ":" + "" + j);
+                }
+            }
+        return ans; 
+    }
+}
