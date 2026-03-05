03fae63c56612df0f0fa9a0e17633c9abe481597
Prince <rana.prince1806@gmail.com>
2026-03-05 23:34:44 +0530

    Add files via upload

---
+++ leetcode1758.java
@@ -0,0 +1,12 @@
+// public class leetcode1758 {
+    
+// }
+class Solution {
+    public int minOperations(String s) {
+      int count=0,n=s.length();
+      for(int i=0;i<n;i++){
+        count+=(s.charAt(i)^i) & 1;
+      }  
+      return Math.min(count,n-count);
+    }
+}
