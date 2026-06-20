92a9eebaccaa07682aee1f647359a841ea5b4e61
Prince <rana.prince1806@gmail.com>
2026-06-20 18:26:00 +0530

    Create leetcode1840.java

---
+++ leetcode1840.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maxBuilding(int n, int[][] restrictions) {
+         List<int[]> list = new ArrayList<>();
+
+        list.add(new int[]{1, 0});
+
+        for (int[] r : restrictions) {
+            list.add(new int[]{r[0], r[1]});
+        }
+
+        list.add(new int[]{n, n - 1});
+
+        list.sort((a, b) -> Integer.compare(a[0], b[0]));
+
+        int m = list.size();
+
+        for (int i = 1; i < m; i++) {
+            list.get(i)[1] = Math.min(
+                list.get(i)[1],
+                list.get(i - 1)[1] + (list.get(i)[0] - list.get(i - 1)[0])
+            );
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            list.get(i)[1] = Math.min(
+                list.get(i)[1],
+                list.get(i + 1)[1] + (list.get(i + 1)[0] - list.get(i)[0])
+            );
+        }
+
+        long ans = 0;
+
+        for (int i = 1; i < m; i++) {
+            int x1 = list.get(i - 1)[0];
+            int h1 = list.get(i - 1)[1];
+
+            int x2 = list.get(i)[0];
+            int h2 = list.get(i)[1];
+
+            int dist = x2 - x1;
+
+            long peak = (long) (h1 + h2 + dist) / 2;
+            ans = Math.max(ans, peak);
+        }
+
+        return (int) ans;
+    }
+}
